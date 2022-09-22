dd59e08ad2b1a785e37cd9b69acc31bcaff565a8
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-09-22 20:34:24 -0300

    test - create voucher

---
+++ tests/unit/voucher.test.ts
@@ -0,0 +1,10 @@
+import * as voucherService from "../../src/services/voucherService"
+
+describe('[TEST] createVoucher()', () => { 
+
+    it('to do', () => {
+        jest.spyOn(voucherService, 'createVoucher').mockImplementationOnce(() => {
+            return true;
+        })
+    })
+ })
